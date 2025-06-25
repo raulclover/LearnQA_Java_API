26427a52de102112dd5f9944c0f64bd16ff90662
raulclover <raulclover@gmail.com>
2025-06-25 11:50:17 +0300

    commit

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,6 +3,6 @@ import org.junit.jupiter.api.Test;
 public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello from RaulClover");
+        System.out.println("Hello from Raul Clover");
     }
 }
